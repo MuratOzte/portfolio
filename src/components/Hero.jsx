0700e95c19e421da29e14188640f1e56b9b16eac
MuratOzte <mo2553481@gmail.com>
2023-09-23 04:39:55 +0300

    3D canvas added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
+import ThreeSixtyIcon from '@mui/icons-material/ThreeSixty';
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+    return (
+        <div className="relative mx-auto w-full h-screen bg-yellow-100 ">
+            <div
+                className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 justify-between `}
+            >
+                <div className="flex flex-col justify-center items-center mt-5">
+                    <div className="w-5 h-5 rounded-full bg-yellow-500 " />
+                    <div className="w-1 sm:h-80 h-40 yellow-gradient" />
+                </div>
+                <div>
+                    <h1 className={`${styles.heroHeadText} text-gray-800`}>
+                        Hi, I'm <span className="text-[#eab308]">Murat</span>
+                    </h1>
+                    <p className={`${styles.heroSubText} text-gray-600 mt-3 `}>
+                        I build web applications and user interfaces
+                    </p>
+                </div>
+                <div className="w-96 h-96 ml-0">
+                    <ComputersCanvas />
+                    <ThreeSixtyIcon
+                        color="action"
+                        style={{ width: '2em', height: '2em', marginLeft: 172 }}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Hero
+export default Hero;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { useState, useEffect, Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import { HemisphereLight, PointLight } from 'three';
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+    const computer = useGLTF('./desktop_pc/scene.gltf');
+    return (
+        <mesh>
+            <primitive
+                scale={0.8}
+                object={computer.scene}
+                position={[0,-2.8,0]}
+                rotation={[0.0 , Math.PI / 2 , 0]}
+            />
+        </mesh>
+    );
+};
 
-export default Computers
+const ComputersCanvas = () => {
+    return (
+        <Canvas
+            frameloop="always"
+            camera={{ position: [0.5,-10,9.2], fov: 30}}
+            gl={{ preserveDrawingBuffer: true }}
+        >
+            <Suspense>
+                <OrbitControls
+                    enableZoom={true}
+                    maxPolarAngle={Math.PI / 2}
+                    minPolarAngle={Math.PI / 2}
+                />
+                <Computers />
+            </Suspense>
+            <Preload all />
+        </Canvas>
+    );
+};
+
+export default ComputersCanvas;
